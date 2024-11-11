2f7550b7ab541bcdbd244b2b2fa7dd5982d6b555
Fatima Hamdouch <hamdouchfatima168@gmail.com>
2024-11-11 11:55:08 +0200

    other numbers in range

--- index.py
+++ index.py
@@ -6,11 +6,18 @@ def moveZeroes(nums):
         if nums[i] > 0:
             nums[zeroes], nums[i] = nums[i], nums[zeroes]
             zeroes = zeroes + 1
+    len_no0 = len(nums)-zeroes-1
+    for i in range(len_no0):
+        if(nums[i]>nums[i+1]):
+            temp=nums[i]
+            nums[i]=nums[i+1]
+            nums[i+1]=temp
+
     return nums
 
-#nums = [0, 1, 0, 3, 12]
-#print(moveZeroes(nums))
-print(moveZeroes([34,2,0,5,0,7,0,0]))
+nums = [0, 1, 0, 3, 12]
+print(moveZeroes(nums))
+#print(moveZeroes([34,2,0,5,0,7,0,0]))
 
 
             
